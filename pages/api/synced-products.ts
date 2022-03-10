1504a8355b55d2e26a9c6a64412969258dc5d7f6
Russell Brian Fulache Dugaduga Jale Palma <rpalm@Russells-MacBook-Pro.local>
2022-03-09 23:07:48 -0800

    removing empty exports

---
+++ pages/api/synced-products.ts
@@ -0,0 +1,22 @@
+// import type { NextApiRequest, NextApiResponse } from 'next';
+
+// type Data = {
+//   success: boolean;
+// };
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//     if (req.method === 'POST') {
+//         res.status(200).json({ success: true });
+//     }
+
+//     if (req.method === 'GET') {
+//         res.status(200).json({ success: true });
+//     }
+// //   console.log(req.body);
+// //   res.status(200).json({ success: true });
+// }
+
+export {};
